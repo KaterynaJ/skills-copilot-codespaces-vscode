9743d312d6a94ca54303cb6db2ae50ac590fc011
Kateryna <114310883+KaterynaJ@users.noreply.github.com>
2024-08-06 18:56:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,66 @@
-// Сreate web server and return comments in JSON format
-var http = require('http');
-var fs = require('fs');
+// Сreate web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
 
-// Create server
-http.createServer(function (req, res) {
-    // Read file with comments
-    fs.readFile('comments.json', 'utf8', function (err, data) {
+const comments = require('./comments');
+
+const server = http.createServer((req, res) => {
+  const urlParsed = url.parse(req.url, true);
+
+  switch (urlParsed.pathname) {
+    case '/':
+      sendFile('index.html', res);
+      break;
+
+    case '/comments':
+      comments.getComments((err, data) => {
         if (err) {
-            console.log('Error: ' + err);
-            return;
+          res.statusCode = 500;
+          res.end('Server error');
+          return;
         }
-        // Send comments in JSON format
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(data);
-    });
-}).listen(8080);
+        res.end(JSON.stringify(data));
+      });
+      break;
 
-console.log('Server running on port 8080.');
+    case '/submit':
+      let body = '';
+      req
+        .on('data', (chunk) => {
+          body += chunk.toString();
+        })
+        .on('end', () => {
+          const data = JSON.parse(body);
+          comments.addComment(data, (err) => {
+            if (err) {
+              res.statusCode = 500;
+              res.end('Server error');
+              return;
+            }
+            res.end('Ok');
+          });
+        });
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.end('Not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server started');
+});
+
+function sendFile(fileName, res) {
+  const fileStream = fs.createReadStream(path.join(__dirname, fileName));
+  fileStream.pipe(res);
+
+  fileStream.on('error', (err) => {
+    res.statusCode = 500;
+    res.end('Server error');
+    console.error(err);
+  });
+}
